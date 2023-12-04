ad4e1283c9c369de49b254b781d46b59a9e1eb58
Basant Morsi <basant179@gmail.com>
2023-12-04 15:54:23 +0100

    created project with devise gem

---
+++ db/migrate/20231204143148_add_columns_to_user.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUser < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :username, :string
+    add_column :users, :address, :string
+    add_column :users, :about_me, :text
+  end
+end
