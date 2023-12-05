63ab24c16fdfe9f92a3da78c0c26728713926e52
Youri Soposki <youri.soposki@gmail.com>
2023-12-05 14:17:04 +0100

    first draft tool show page

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,4 +1,10 @@
 class ToolsController < ApplicationController
   def index
+    @tools = Tool.all
+  end
+
+  def show
+    @tool = Tool.find(params[:id])
+    @user = @tool.user if @tool
   end
 end
