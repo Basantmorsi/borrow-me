6afd0506d0ff504013bf574765b73b30ccc393b4
Basant Morsi <basant179@gmail.com>
2023-12-05 16:27:39 +0100

    dashboard tabs

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,7 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
+  static targets = ["tab", "cards"]
   connect() {
     console.log("toggle controller")
+  }
+  toggle(event) {
+    const selected = document.querySelector('.selected');
+    if (selected !== null) {
+      selected.classList.toggle('selected');
+    }
+    event.target.classList.toggle('selected');
+
+    this.request = new Request(event.target.dataset.toggleUrl);
+    this.fetchContent(this.request);
+  }
+
+  fetchContent(request) {
+    fetch(request)
+      .then((response) => {
+        if (response.status == 200) {
+          response.text().then((text) => this.cardsTarget.innerHTML = text);
+        } else {
+          console.log("Couldn't load data");
+        }
+      })
   }
 }
