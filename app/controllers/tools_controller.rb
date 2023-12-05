5d62b22e53bce52b3603fb9c1b7288c040da957d
Basant Morsi <basant179@gmail.com>
2023-12-05 13:00:03 +0100

    create a new tool

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,4 +1,26 @@
 class ToolsController < ApplicationController
   def index
   end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    @tool = Tool.new(tool_params)
+    @tool.user_id = current_user.id
+    if @tool.save
+      redirect_to tools_path, notice: 'Tool was successfully created.'
+    else
+      flash.now[:alert] = 'Failed to create a tool. Please fix the errors below.'
+      render :new
+    end
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name, :description, :availability, :manual, :brand)
+  end
+
 end
