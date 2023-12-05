03d5edc23bd6833f3fe0e08a8f6e118e2f8deaa7
Medard Kuklis <medocius@gmail.com>
2023-12-05 12:23:31 +0100

    added seed

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,4 +1,4 @@
 class Tool < ApplicationRecord
-  belongs_to :user_id
+  belongs_to :user
   validates :name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+ToolRequest.destroy_all
+Tool.destroy_all
+
+30.times do |i|
+  Tool.create!(
+    name: "Tool #{i + 1}",
+    user_id: 1,
+    description: "Description for Tool #{i + 1}",
+    availability: [true, false].sample,
+    manual: "Manual for Tool #{i + 1}",
+    brand: "Brand #{i + 1}"
+  )
+end
