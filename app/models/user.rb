7f5d9017a7d2398db04e68ae93f049573cfcd09b
Basant Morsi <basant179@gmail.com>
2023-12-06 11:55:31 +0100

    geocoding the address

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
          validates :address, presence: true
          validates :first_name, presence: true
          validates :last_name, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
