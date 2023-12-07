fed23d3f19b36bbe4e73b95ebc14e0b691c7b893
Basant Morsi <basant179@gmail.com>
2023-12-07 14:50:49 +0100

    creating messages model

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,5 @@
 class Chatroom < ApplicationRecord
   belongs_to :sender, :class_name => 'User'
   belongs_to :recipient, :class_name => 'User'
+  has_many :messages
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_07_134433) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_07_134735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_134433) do
     t.index ["recipient_id"], name: "index_chatrooms_on_recipient_id"
     t.index ["sender_id"], name: "index_chatrooms_on_sender_id"
     t.index ["tool_id"], name: "index_chatrooms_on_tool_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "tool_requests", force: :cascade do |t|
@@ -70,6 +80,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_134433) do
   end
 
   add_foreign_key "chatrooms", "tools"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "tool_requests", "tools"
   add_foreign_key "tool_requests", "users"
   add_foreign_key "tools", "users"
