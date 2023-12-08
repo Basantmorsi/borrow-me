c087ef4d36163f4e7123c3d5b18ff294871f616a
Youri Soposki <youri.soposki@gmail.com>
2023-12-08 12:17:48 +0100

    cloudinary installation for seeds

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -9,7 +9,7 @@ class ToolsController < ApplicationController
       {
         lng: tool.user.longitude,
         lat: tool.user.latitude,
-        info_window_html: render_to_string(partial: "info_window", locals: { user: tool.user })
+        # info_window_html: render_to_string(partial: "info_window", locals: { user: tool.user })
       }
     end
 
@@ -84,7 +84,7 @@ class ToolsController < ApplicationController
   private
 
   def tool_params
-    params.require(:tool).permit(:name, :description, :availability, :manual, :brand)
+    params.require(:tool).permit(:name, :description, :availability, :manual, :photo, :brand)
   end
 
 end
--- app/models/tool.rb
+++ app/models/tool.rb
@@ -3,4 +3,5 @@ class Tool < ApplicationRecord
   has_many :tool_requests
   validates :name, presence: true
   scope :search_by_name, ->(query) { where('name ILIKE ?', "%#{query}%") }
+  has_one_attached :photo
 end
