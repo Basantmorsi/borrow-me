cca0b1ad29e5dedaf1930ca750d43da083934dba
Youri Soposki <youri.soposki@gmail.com>
2023-12-08 11:50:08 +0100

    first last name remove

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_07_134735) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_08_104700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,8 +76,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_134735) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
     t.string "username"
     t.string "address"
     t.text "about_me"
