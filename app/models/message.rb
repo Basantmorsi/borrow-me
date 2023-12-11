09a6426713f37d649d5bbc101194bcb5918dfef2
Basant Morsi <basant179@gmail.com>
2023-12-11 17:30:46 +0100

    chat modifications

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,12 @@
 class Message < ApplicationRecord
   belongs_to :chatroom
   belongs_to :user
+  belongs_to :tool, optional: true
+  belongs_to :request, optional: true
+
   validates :content, presence: true, length: {minimum: 2, maximum: 1000}
+
+  def topic
+    tool_id.nil? ? request.message : tool.name
+  end
 end
