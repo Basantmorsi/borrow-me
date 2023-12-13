aa9e247cad172e8b4667519accc058d6df2369d0
Basant Morsi <basant179@gmail.com>
2023-12-13 12:00:21 +0100

    style

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,6 +2,10 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.includes(:messages).find(params[:id])
     @message = Message.new
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: "chatrooms/show", locals: {chatroom: @chatroom}, formats: [:html] }
+    end
   end
 
   def index
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,6 +7,28 @@ class DashboardController < ApplicationController
     # You may want to include additional conditions or sorting based on your requirements
     # For example, you can order the requests by creation time:
     @tool_requests = @tool_requests.order(created_at: :desc)
+
+    #Select Chatroom for the user
+    @chats_users = []
+    @chatrooms = Chatroom.where(sender_id: current_user.id).
+    or(Chatroom.where(recipient_id: current_user.id))
+    # .joins("INNER JOIN users ON users.id = chatrooms.sender_id OR users.id = chatrooms.recipient_id")
+    if @chatrooms.present?
+      @chatrooms.each do |chatroom|
+        if chatroom.sender_id === current_user.id
+
+          #[#<User id: 2, email: "jan@jan.com", created_at: "2023-12-07 14:02:48.346178000 +0000",
+          # updated_at: "2023-12-07 14:02:48.346178000 +0000", username: "jan.doe",
+          # address: "dresden germany", about_me: nil, latitude: 51.0493286, longitude: 13.7381437>
+          # user = User.find(chatroom.recipient_id)
+          # @chats_users << {}
+          @chats_users << User.find(chatroom.recipient_id)
+        else
+          @chats_users << User.find(chatroom.sender_id)
+        end
+      end
+    end
+
     respond_to do |format|
       format.html # Follow regular flow of Rails
       format.text { render partial: "tool_requests/index", locals: {tool_requests: @tool_requests}, formats: [:html] }
